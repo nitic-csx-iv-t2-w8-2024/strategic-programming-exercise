9ed5d7c0c481e928af075c42dbddff5ca7d9a332
shio <85730998+dino3616@users.noreply.github.com>
2024-11-15 15:09:15 +0000

    feat: ✨ (prisoners-dilemma/strategy) Equalizer ZD 戦略を実装

--- src/prisoners-dilemma/strategy/strategy.h
+++ src/prisoners-dilemma/strategy/strategy.h
@@ -1,11 +1,12 @@
 #ifndef PRISONERS_DILEMMA_STRATEGY_H
 #define PRISONERS_DILEMMA_STRATEGY_H
 
+extern int prisoners_dilemma_equalizer_zd_strategy(int ID, int n, int SC[2],
+                                                   int *H);
+extern int prisoners_dilemma_extortionate_zd_strategy(int ID, int n, int SC[2],
+                                                      int *H);
 extern int prisoners_dilemma_generous_zd_strategy(int ID, int n, int SC[2],
                                                   int *H);
 extern int prisoners_dilemma_random_strategy(int ID, int n, int SC[2], int *H);
-
-extern int prisoners_dilemma_extortionate_zd_strategy(int ID, int n, int SC[2],
-                                                      int *H);
 
 #endif
